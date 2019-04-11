ab4f9dd6124e7995a70dfa439ee2164e3e7b5028
Daniel D. Fernandes <daniel.dourado@gmail.com>
2019-04-11 20:59:58 -0300

    CarFrameMoveInfo added

---
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -0,0 +1,74 @@
+using System;
+using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+using System.Collections.Generic;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof (CarController))]
+    public class CarUserControl : MonoBehaviour
+    {
+        private CarController m_Car; // the car controller we want to use
+        private List<CarFrameMoveInfo> m_CarFrameMoveInfos;
+        private CarFrameMoveInfo m_CurrentFrameMoveInfo;
+        public Vector3 CurrentFrameMoveInfo;
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+            m_CarFrameMoveInfos = new List<CarFrameMoveInfo>();
+            m_CarFrameMoveInfos.Add(new CarFrameMoveInfo());
+
+        }
+
+        private void FixedUpdate()
+        {
+            m_CurrentFrameMoveInfo = new CarFrameMoveInfo();
+            m_CurrentFrameMoveInfo.AddFrameMoveInfo(m_CarFrameMoveInfos[m_CarFrameMoveInfos.Count - 1]);
+            m_CarFrameMoveInfos.Add(m_CurrentFrameMoveInfo);
+            
+            m_Car.Move(m_CurrentFrameMoveInfo.h, m_CurrentFrameMoveInfo.v, m_CurrentFrameMoveInfo.v, 0);
+            SetCurrentFrameMoveInfo(m_CurrentFrameMoveInfo);
+        }
+
+        private void SetCurrentFrameMoveInfo(CarFrameMoveInfo carFrameMoveInfo)
+        {
+            CurrentFrameMoveInfo = new Vector3(carFrameMoveInfo.h, carFrameMoveInfo.v, carFrameMoveInfo.handbrake);
+        }
+    }
+
+    public class CarFrameMoveInfo
+    {
+        private float m_MinClamp = -2f;
+        private float m_MaxClamp = 2f;
+
+        public float h;
+        public float v;
+        public float handbrake;
+
+        public CarFrameMoveInfo()
+        {
+            SetRandomFrameMoveInfo();
+        }
+
+        public void SetRandomFrameMoveInfo() 
+        {
+            h = UnityEngine.Random.Range(-1f, 1f);
+            v = UnityEngine.Random.Range(-1f, 1f);
+            handbrake = UnityEngine.Random.Range(-1f, 1f);
+        }
+
+        public void AddFrameMoveInfo(CarFrameMoveInfo carFrameMoveInfo)
+        {
+            this.h += carFrameMoveInfo.h;
+            this.v += carFrameMoveInfo.v;
+            this.handbrake += carFrameMoveInfo.handbrake;
+
+            this.h = Mathf.Clamp(this.h, m_MinClamp, m_MaxClamp);
+            this.v = Mathf.Clamp(this.v, m_MinClamp, m_MaxClamp);
+            this.handbrake = Mathf.Clamp(this.handbrake, m_MinClamp, m_MaxClamp);
+        }
+
+    }
+}
