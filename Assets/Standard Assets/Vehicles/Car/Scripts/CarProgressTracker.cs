14168dd7e1146112c71563cdd989238020a022cb
Daniel D. Fernandes <daniel.dourado@gmail.com>
2019-04-18 18:45:42 -0300

    system to track progress on the track

---
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarProgressTracker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarProgressTracker : MonoBehaviour
+{
+
+    private List<GameObject> gatesTriggered = new List<GameObject>();
+    private ProgressTracker m_ProgressTracker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ProgressTracker = GameObject.FindObjectOfType<ProgressTracker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private float GetProgressOnTrack()
+    {
+        return 0f;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        StrategicGate tempGate = collider.gameObject.GetComponent<StrategicGate>();
+        if (tempGate)
+        {
+            if (!gatesTriggered.Contains(tempGate.gameObject))
+                gatesTriggered.Add(tempGate.gameObject);
+        }
+    }
+
+    private float CalculateFitness()
+    {
+        return m_ProgressTracker.CalculateProgress(gatesTriggered, transform.position);
+    }
+}
---
+++ Assets/Standard Assets/Vehicles/Car/Scripts/ProgressTracker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressTracker : MonoBehaviour
+{
+    private float m_TotalDistance = 0;
+    void Start()
+    {
+        List<GameObject> gates = GetAllChildrenGameobjects();
+        m_TotalDistance = GetTotalTrackDistanceFromListOfGameobjects(gates);
+    }
+
+    private float GetTotalTrackDistanceFromListOfGameobjects(List<GameObject> gameObjectList)
+    {
+        float totalDistance = 0f;
+
+        for(int i=0; i<gameObjectList.Count; i++)
+        {
+            int nextIndex = GetNextIndexForGate(i, gameObjectList);
+
+            float tempDistance = Vector3.Distance(  gameObjectList[i].transform.position, 
+                                                gameObjectList[nextIndex].transform.position);
+            totalDistance += tempDistance;
+
+        }
+
+        return totalDistance;
+    }
+
+    private int GetNextIndexForGate(int currentIndex, List<GameObject> list)
+    {
+        int nextIndex = currentIndex+1;
+        if(nextIndex >= list.Count)
+            nextIndex = 0;
+        return nextIndex;
+    }
+
+    List<GameObject> GetAllChildrenGameobjects()
+    {
+        List<GameObject> list = new List<GameObject>();
+        foreach (Transform child in transform)
+        {
+            list.Add(child.gameObject);
+        }
+        return list;
+    }
+
+    public float CalculateProgress(List<GameObject> gatesTriggered, Vector3 position)
+    {
+        float progress = 0f;
+
+        GameObject lastGateTriggered = gatesTriggered[gatesTriggered.Count - 1];
+        float partialDistance = GetDistanceFromListOfGameobjects(gatesTriggered);
+        partialDistance += Vector3.Distance(lastGateTriggered.transform.position, position);
+
+        progress = partialDistance / m_TotalDistance;
+
+        return progress;
+    }
+
+    private float GetDistanceFromListOfGameobjects(List<GameObject> gameObjectList)
+    {
+        float totalDistance = 0f;
+
+        for(int i=0; i<gameObjectList.Count; i++)
+        {
+            int nextIndex = GetNextIndexForGate(i, gameObjectList);
+
+            if (nextIndex == 0) break;
+
+            float tempDistance = Vector3.Distance(gameObjectList[i].transform.position,
+                                                gameObjectList[nextIndex].transform.position);
+            totalDistance += tempDistance;
+        }
+
+        return totalDistance;
+    }
+}
