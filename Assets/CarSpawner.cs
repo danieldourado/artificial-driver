9e7ea01854cd631186f2e8fd9c263df59cf45dc4
Daniel D. Fernandes <daniel.dourado@gmail.com>
2019-04-19 00:50:46 -0300

    spawning multiple cars that don't colide with eachother

---
+++ Assets/CarSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject CarPrefab;
+    public int numberOfInstances = 5;
+    void Start()
+    {
+        for (int i=0;i< numberOfInstances; i++)
+        {
+            GameObject tempCar = GameObject.Instantiate(CarPrefab);
+            tempCar.transform.SetParent(transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
